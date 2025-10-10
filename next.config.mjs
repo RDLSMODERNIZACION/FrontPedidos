2e1baff129a81a75ece6740d471ff21594d20355
DIRAC <energiadirac@gmail.com>
2025-10-10 13:15:20 -0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  swcMinify: true,
+  // No configures distDir ni output: 'export'
 };
+
 export default nextConfig;
